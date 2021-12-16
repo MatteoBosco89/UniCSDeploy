2588abd4b9436afe73823ca402d9c2bbf677eea1
SenselessMite <pasqualecavoto1996@gmail.com>
2021-12-16 15:49:10 +0100

    Added Security Affairs Connector

---
+++ Connectors/SecurityAffairs/src/scrap/Scraper.py
@@ -0,0 +1,32 @@
+import cloudscraper
+from bs4 import BeautifulSoup
+
+class Scraper:
+
+    def __init__(self):
+        self.feed_url = "https://securityaffairs.co/wordpress/feed"
+
+    def getAllArticles(self):
+        scraper = cloudscraper.create_scraper()
+        feed = scraper.get(self.feed_url)
+        articles_list = []
+        file = open("data.json", "w")
+        if "200" in str(feed):
+            soup = BeautifulSoup(feed.text, "lxml")
+            articles = soup.findAll("item")
+
+            for art in articles:
+                title = art.find("title").text
+                description = art.find("description").text
+                link = art.findAll("a")[0].get("href")
+
+                data = {
+                    "title" = title,
+                    "description" = description,
+                    "link" = link
+                }
+                articles_list.append(data)
+
+            return articles_list
+        else:
+            return None
